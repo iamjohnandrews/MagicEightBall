67ddc1d134e35b9ba229fa040051957fd2dcfa00
John Andrews <johnnydrews@gmail.com>
2013-11-12 12:49:01 -0600

    added button and label to storyboard

---
+++ MagicEightBall2.0/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  MagicEightBall2.0
+//
+//  Created by John Andrews on 11/12/13.
+//  Copyright (c) 2013 John Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *answerDisplayLabel;
+
+@property (weak, nonatomic) IBOutlet UIButton *shakeMagicEightBallButton;
+
+@end
